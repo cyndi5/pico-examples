7a9e374297d913314d4c2ff957a2ec7dfa49fba3
Cyndi Cavanaugh <cynthiacavanaugh1@gmail.com>
2023-01-15 16:59:48 -0600

    initial

---
+++ cynthia/timing/timing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    uint subs = 33;
+    printf("Hi.\n");
+    while (true) {
+        printf("Cynthia: Hunter of Knowledge. Subscribers: %d\n", subs);
+        sleep_ms(1000);
+        subs++;
+    }
+    return 0;
+}
