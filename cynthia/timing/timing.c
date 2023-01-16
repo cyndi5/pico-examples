57bc0b1f75aa13e3d55df3415aeb3e9e574e5080
Cyndi Cavanaugh <cynthiacavanaugh1@gmail.com>
2023-01-15 21:36:12 -0600

    timing

--- cynthia/timing/timing.c
+++ cynthia/timing/timing.c
@@ -4,9 +4,23 @@
 int main() {
     stdio_init_all();
     uint subs = 33;
-    printf("Hi.\n");
+
+    sleep_ms(875);
+    printf("seems to get lost\n");
+    printf("seems to get lost as well\n");
+    sleep_ms(1000);
+
+    // printf("Hi 1.\n");
+    // sleep_ms(1000);
+
+    // printf("Hi 2.\n");
+    // sleep_ms(1000);
+
+    // printf("Hi 3.\n");
+    // sleep_ms(1000);
+
     while (true) {
-        printf("Cynthia: Hunter of Knowledge. Subscribers: %d\n", subs);
+        printf("Cynthia: Hunter of Knowledge! Subscribers: %d\n", subs);
         sleep_ms(1000);
         subs++;
     }
